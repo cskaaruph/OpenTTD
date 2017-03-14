3f00b582fa7e857ae1d78305b06e7dd1f36489f2
Jesús A. Álvarez <zydeco@namedfork.net>
2017-03-14 22:25:36 +0100

    add OpenGL video

--- src/video/cocoa_touch_v.mm
+++ src/video/cocoa_touch_v.mm
@@ -13,6 +13,11 @@
 #ifdef WITH_METAL
 #import <Metal/Metal.h>
 #endif
+#ifdef WITH_OPENGL
+#import <OpenGLES/gltypes.h>
+#import <OpenGLES/ES2/gl.h>
+#import <OpenGLES/ES2/glext.h>
+#endif
 #include "stdafx.h"
 #import "cocoa_touch_v.h"
 
@@ -36,6 +41,23 @@ static id<MTLRenderPipelineState> pipelineState = nil;
 static id<MTLBuffer> vertexBuffer = nil;
 static id<MTLBuffer> screenBuffer = nil;
 static id<MTLTexture> screenTexture = nil;
+#endif
+
+#ifdef WITH_OPENGL
+static EAGLContext *glContext = nil;
+static GLuint positionSlot = 0;
+static GLuint texcoordSlot = 0;
+static GLuint textureUniform = 0;
+static GLuint glVertexBuffer = 0;
+static GLuint glScreenTexture = 0;
+static GLuint framebuffer;
+static GLuint renderbuffer;
+static CGRect lastBounds;
+
+typedef struct gl_vertex {
+	float position[3];
+	float texcoord[2];
+} gl_vertex;
 #endif
 
 extern "C" {
@@ -68,6 +90,110 @@ const char *VideoDriver_CocoaTouch::Start(const char * const *parm)
 		}
 	}
 #endif
+	
+#if WITH_OPENGL
+	if (_cocoa_touch_layer == NULL) {
+		glContext = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
+		CAEAGLLayer *eaglLayer = NULL;
+		if (glContext != nil) {
+			eaglLayer = [CAEAGLLayer layer];
+			eaglLayer.opaque = YES;
+			eaglLayer.contentsScale = [UIScreen mainScreen].nativeScale;
+		} else {
+			NSLog(@"Error initializing context");
+			goto opengl_fail;
+		}
+		
+		__block NSError *error = nil;
+		
+		if (![EAGLContext setCurrentContext:glContext]) {
+			NSLog(@"Error setting current context");
+			goto opengl_fail;
+		}
+		
+		GLuint (^compile)(NSString *, GLenum) = ^(NSString *name, GLenum type) {
+			NSString *path = [[NSBundle mainBundle] pathForResource:name ofType:@"glsl"];
+			NSString *string = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:&error];
+			if (!string) {
+				NSLog(@"Error loading shader \"%@\": %@", name, error.localizedDescription);
+				return (GLuint)0;
+			}
+			
+			GLuint handle = glCreateShader(type);
+			
+			const GLchar *program = [string UTF8String];
+			const GLint length = (GLint)string.length;
+			glShaderSource(handle, 1, &program, &length);
+			glCompileShader(handle);
+			
+			GLint success;
+			glGetShaderiv(handle, GL_COMPILE_STATUS, &success);
+			if (success == GL_FALSE) {
+				GLchar messages[256];
+				glGetShaderInfoLog(handle, sizeof(messages), 0, messages);
+				NSLog(@"%@", @(messages));
+				return (GLuint)0;
+			}
+			
+			return handle;
+		};
+		
+		glGenRenderbuffers(1, &renderbuffer);
+		glGenFramebuffers(1, &framebuffer);
+		
+		GLuint vertexShader = compile(@"Vertex", GL_VERTEX_SHADER);
+		GLuint fragmentShader = compile(@"Fragment", GL_FRAGMENT_SHADER);
+		if (vertexShader == 0 || fragmentShader == 0)
+			goto opengl_fail;
+		
+		GLuint program = glCreateProgram();
+		glAttachShader(program, vertexShader);
+		glAttachShader(program, fragmentShader);
+		glLinkProgram(program);
+		
+		GLint success;
+		glGetProgramiv(program, GL_LINK_STATUS, &success);
+		if (success == GL_FALSE) {
+			GLchar messages[256];
+			glGetProgramInfoLog(program, sizeof(messages), 0, messages);
+			NSLog(@"%@", @(messages));
+			goto opengl_fail;
+		}
+		
+		glUseProgram(program);
+		
+		positionSlot = glGetAttribLocation(program, "position");
+		texcoordSlot = glGetAttribLocation(program, "texcoord_in");
+		glEnableVertexAttribArray(positionSlot);
+		glEnableVertexAttribArray(texcoordSlot);
+		
+		textureUniform = glGetUniformLocation(program, "texture");
+		
+		const gl_vertex vertices[] = {
+			{{-1, -1, 0}, {0, 1}},
+			{{-1, 1, 0}, {0, 0}},
+			{{1, -1, 0}, {1, 1}},
+			{{1, 1, 0}, {1, 0}}
+		};
+		glGenBuffers(1, &glVertexBuffer);
+		glBindBuffer(GL_ARRAY_BUFFER, glVertexBuffer);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		
+		glGenTextures(1, &glScreenTexture);
+		glBindTexture(GL_TEXTURE_2D, glScreenTexture);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glBindTexture(GL_TEXTURE_2D, 0);
+		
+		[EAGLContext setCurrentContext:nil];
+		_cocoa_touch_layer = eaglLayer;
+	}
+opengl_fail:
+#endif
+	
 	if (_cocoa_touch_layer == NULL) {
 		_cocoa_touch_layer = [CALayer layer];
 	}
@@ -79,14 +205,8 @@ const char *VideoDriver_CocoaTouch::Start(const char * const *parm)
 
 void VideoDriver_CocoaTouch::Stop()
 {
-	if (this->context) {
-		CGContextRelease(this->context);
-	}
-	if (this->pixel_buffer) {
-		free(this->pixel_buffer);
-		this->pixel_buffer = NULL;
-	}
 	_cocoa_touch_driver = NULL;
+	
 #ifdef WITH_METAL
 	if (commandQueue) {
 		commandQueue = nil;
@@ -96,6 +216,28 @@ void VideoDriver_CocoaTouch::Stop()
 		screenTexture = nil;
 	}
 #endif
+	
+#ifdef WITH_OPENGL
+	if (glContext) {
+		[EAGLContext setCurrentContext:glContext];
+		glDeleteTextures(1, &glScreenTexture);
+		glDeleteBuffers(1, &glVertexBuffer);
+		positionSlot = 0;
+		texcoordSlot = 0;
+		textureUniform = 0;
+		[EAGLContext setCurrentContext:nil];
+		glContext = nil;
+	}
+#endif
+	
+	if (this->context) {
+		CGContextRelease(this->context);
+	}
+	if (this->pixel_buffer) {
+		free(this->pixel_buffer);
+		this->pixel_buffer = NULL;
+	}
+	_cocoa_touch_layer = nil;
 }
 
 void VideoDriver_CocoaTouch::ExitMainLoop()
@@ -131,8 +273,9 @@ bool VideoDriver_CocoaTouch::ChangeResolution(int w, int h)
 	_screen.dst_ptr = pixel_buffer;
 	
 #ifdef WITH_METAL
-	CAMetalLayer *metalLayer = ([_cocoa_touch_layer isKindOfClass:[CAMetalLayer class]] ? (CAMetalLayer *)_cocoa_touch_layer : nil);
-	if (metalLayer) {
+	if ([_cocoa_touch_layer isKindOfClass:[CAMetalLayer class]]) {
+		CAMetalLayer *metalLayer = (CAMetalLayer *)_cocoa_touch_layer;
+		
 		if (commandQueue) {
 			commandQueue = nil;
 			pipelineState = nil;
@@ -177,6 +320,14 @@ bool VideoDriver_CocoaTouch::ChangeResolution(int w, int h)
 	}
 #endif
 	
+#ifdef WITH_OPENGL
+	if ([_cocoa_touch_layer isKindOfClass:[CAEAGLLayer class]]) {
+		BlitterFactory::GetCurrentBlitter()->PostResize();
+		GameSizeChanged();
+		return true;
+	}
+#endif
+	
 	// default to CoreGraphics
 	int bitsPerComponent = 8;
 	int bitsPerPixel = 32;
@@ -188,7 +339,6 @@ bool VideoDriver_CocoaTouch::ChangeResolution(int w, int h)
 	}
 	this->context = CGBitmapContextCreate(pixel_buffer, _screen.width, _screen.height, bitsPerComponent, bytesPerRow, colorSpace, options);
 	CGColorSpaceRelease(colorSpace);
-	
 	
 	BlitterFactory::GetCurrentBlitter()->PostResize();
 	GameSizeChanged();
@@ -213,8 +363,8 @@ void VideoDriver_CocoaTouch::EditBoxLostFocus()
 void VideoDriver_CocoaTouch::Draw()
 {
 #ifdef WITH_METAL
-	CAMetalLayer *metalLayer = ([_cocoa_touch_layer isKindOfClass:[CAMetalLayer class]] ? (CAMetalLayer *)_cocoa_touch_layer : nil);
-	if (metalLayer) {
+	if ([_cocoa_touch_layer isKindOfClass:[CAMetalLayer class]]) {
+		CAMetalLayer *metalLayer = (CAMetalLayer *)_cocoa_touch_layer;
 		if (CGSizeEqualToSize(metalLayer.drawableSize, CGSizeZero)) {
 			NSLog(@"The drawable's size is empty");
 			return;
@@ -243,6 +393,51 @@ void VideoDriver_CocoaTouch::Draw()
 		[commandBuffer presentDrawable:drawable];
 		[commandBuffer commit];
 		[commandBuffer waitUntilCompleted];
+		return;
+	}
+#endif
+	
+#ifdef WITH_OPENGL
+	if ([_cocoa_touch_layer isKindOfClass:[CAEAGLLayer class]]) {
+		CAEAGLLayer *eaglLayer = (CAEAGLLayer *)_cocoa_touch_layer;
+		if (![EAGLContext setCurrentContext:glContext])
+			return;
+		
+		glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+		glBindRenderbuffer(GL_RENDERBUFFER, renderbuffer);
+		
+		CGRect bounds = eaglLayer.bounds;
+		CGFloat scale = eaglLayer.contentsScale;
+		
+		if (!CGRectEqualToRect(lastBounds, bounds)) {
+			glDeleteRenderbuffers(1, &renderbuffer);
+			glGenRenderbuffers(1, &renderbuffer);
+			glBindRenderbuffer(GL_RENDERBUFFER, renderbuffer);
+			[glContext renderbufferStorage:GL_RENDERBUFFER fromDrawable:eaglLayer];
+			glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, renderbuffer);
+			lastBounds = bounds;
+		}
+		
+		glViewport(CGRectGetMinX(bounds) * scale, CGRectGetMinY(bounds) * scale, CGRectGetWidth(bounds) * scale, CGRectGetHeight(bounds) * scale);
+		
+		glBindBuffer(GL_ARRAY_BUFFER, glVertexBuffer);
+		glVertexAttribPointer(positionSlot, 3, GL_FLOAT, GL_FALSE, sizeof(gl_vertex), 0);
+		glVertexAttribPointer(texcoordSlot, 2, GL_FLOAT, GL_FALSE, sizeof(gl_vertex), (GLvoid *)(sizeof(float) * 3));
+		
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, glScreenTexture);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, _screen.width, _screen.height, 0, GL_BGRA, GL_UNSIGNED_BYTE, pixel_buffer);
+		glUniform1i(textureUniform, 0);
+		
+		glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glBindTexture(GL_TEXTURE_2D, 0);
+		
+		[glContext presentRenderbuffer:GL_RENDERBUFFER];
+		
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+		glBindRenderbuffer(GL_RENDERBUFFER, 0);
+		[EAGLContext setCurrentContext:nil];
 		return;
 	}
 #endif
